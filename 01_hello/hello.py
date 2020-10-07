bb05b217372fe9098783db51431c9a6349e6f898
Mario Dubreuil <mario_dubreuil@hotmail.com>
2020-10-07 10:56:48 -0400

    format code

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
+""" Say hello """
 
 import argparse
 
+
 def get_args():
+    """ process arguments """
+
     parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
     return parser.parse_args()
 
+
 def main():
+    """ main function """
+
     args = get_args()
     name = args.name
     print("Hello, " + name + "!")
+
 
 if __name__ == '__main__':
     main()
