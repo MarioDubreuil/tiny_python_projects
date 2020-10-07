5c7adba2caee90910d03a0870731da80d330701c
Mario Dubreuil <mario_dubreuil@hotmail.com>
2020-10-07 12:03:36 -0400

    initial version

---
+++ 02_crowsnest/crowsnest.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Author : mario <mario@localhost>
+Date   : 2020-10-06
+Purpose: Crow's nest
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Crow''s nest',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('name',
+                        metavar='str',
+                        help='A positional argument')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Main"""
+
+    args = get_args()
+    name = args.name
+    article = 'a'
+
+    if name[0].lower() in ['a', 'e', 'i', 'o', 'u'] :
+        article += 'n'
+
+    print(f'Ahoy, Captain, {article} {name} off the larboard bow!')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
